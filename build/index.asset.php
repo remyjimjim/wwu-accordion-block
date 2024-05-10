f78c71cd93985e3709981955ad210a321c133879
waltria <waltria@wwu.edu>
2024-05-09 17:30:24 -0700

    init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '18f3487323872f270241');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6de1f35f2ed8fb5d3b30');
